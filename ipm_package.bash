b4677936f4589f619b0af9005dec8f8e6b29883b
M0stafaRady <mostafa.rady@efabless.com>
2025-01-12 14:11:47 +0200

    ip packgae script update to change the yaml file version

--- ipm_package.bash
+++ ipm_package.bash
@@ -38,19 +38,32 @@ done
 echo "+ version=$version"
 
 # zip needed files
-tar czf v$version.tar.gz --exclude='verify' --exclude='hdl/rtl/bus_wrappers/dft' --exclude='hdl/rtl/bus_wrappers/*.dev.v' --exclude='ipm_package.bash' --exclude='fw/Doxyfile' * 
+
+
+tar czf v$version.tar.gz --files-from <(find . | grep -v "\./verify" | grep -v "\./fw/Doxyfile" | grep -v "\./ipm_package.bash" | grep -v ".*\.dev\.v" | grep -v "\./hdl/rtl/bus_wrappers/dft" | grep -v "\.git" | grep -v "\.tar\.gz" | grep -v "\./ip" | grep -v "\./docs" | grep -v "\.\$"; ls "./ip/dependencies.json")
+# tar czf v$version.tar.gz \
+#     --exclude='verify' \
+#     --exclude='hdl/rtl/bus_wrappers/dft' \
+#     --exclude='hdl/rtl/bus_wrappers/*.dev.v' \
+#     --exclude='ipm_package.bash' \
+#     --exclude='fw/Doxyfile' \
+#     --include='*.c' \
+#     *
 # get checksum
 shasum -a 256 v$version.tar.gz > v$version.tar.gz.sha256
 
-# # create tag
-# git tag -a EF_UART-v$version -m "Release version $version"
-# git push origin EF_UART-v$version
+sed -i "s/version.*/version: v$version/" EF_UART.yaml
+sed -i "s/date.*/date: $(date +"%Y-%m-%d")/" EF_UART.yaml
 
-# # create release
-# set -x
-# if gh release view EF_UART-v$version > /dev/null 2>&1; then
-#     echo "Release EF_UART-v$version already exists. Skipping..."
-# else
-#     echo "Creating release EF_UART-v$version..."
-#     gh release create EF_UART-v$version v$version.tar.gz -t "EF_UART-v$version" --notes "sha256: $(cat v$version.tar.gz.sha256)"
-# fi
+# create tag
+git tag -a EF_UART-v$version -m "Release version $version"
+git push origin EF_UART-v$version
+
+# create release
+set -x
+if gh release view EF_UART-v$version > /dev/null 2>&1; then
+    echo "Release EF_UART-v$version already exists. Skipping..."
+else
+    echo "Creating release EF_UART-v$version..."
+    gh release create EF_UART-v$version v$version.tar.gz -t "EF_UART-v$version" --notes "sha256: $(cat v$version.tar.gz.sha256)"
+fi
